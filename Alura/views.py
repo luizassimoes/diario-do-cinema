a6f5447fbf7a2452bbe21c088646b2e2645b5b30
Luiza Souza Simoes <luiza.simoes@qualidados.com.br>
2024-01-16 12:46:35 -0300

    processind_edit + funcao para avg e sd

--- Alura/views.py
+++ Alura/views.py
@@ -1,14 +1,26 @@
 from flask import render_template, request, redirect, session, flash, url_for
-from app import app, db
+from sqlalchemy import desc
 import math
 import pandas as pd
 from datetime import datetime
+
+from app import app, db
 from model.movies import *
 from model.calculations import *
 
+def avg_sd(grade1, grade2):
+    g1 = float(grade1)
+    g2 = float(grade2)
+    avg = (g1 + g2) / 2
+    sd1 = (g1 - avg)**2
+    sd2 = (g2 - avg)**2
+    sd = math.sqrt( (sd1 + sd2) / 2 )
+    return avg, sd
+
+
 @app.route('/')
 def index():
-    movies_query = db.session.query(Movies, Calculations).outerjoin(Calculations, Movies.id == Calculations.id).all()
+    movies_query = db.session.query(Movies, Calculations).outerjoin(Calculations, Movies.id == Calculations.id).order_by(desc(Movies.date)).all()
     return render_template('list.html', title='Locadora', movies=movies_query)
 
 @app.route('/new')
@@ -17,17 +29,14 @@ def new():
 
 @app.route('/processing_new', methods=['POST',])
 def processing_new():
-    movie = request.form['movie']
+    movie = request.form['name']
     date = None if request.form['date'] == '' else request.form['date'] 
     grade1 = None if request.form['grade1'] == '' else float(request.form['grade1'])
     grade2 = None if request.form['grade2'] == '' else float(request.form['grade2'])
 
     if date != None:
         date = datetime.strptime(date, '%d/%m/%Y')
-        avg = (grade1 + grade2) / 2
-        sd1 = (grade1 - avg)**2
-        sd2 = (grade2 - avg)**2
-        sd = math.sqrt( (sd1 + sd2) / 2 )
+        avg, sd = avg_sd(grade1=grade1, grade2=grade2)
 
         new_calculations = Calculations(name=movie, avg=avg, sd=sd)
         db.session.add(new_calculations)
@@ -46,4 +55,20 @@ def edit(id):
 
 @app.route('/processing_edit', methods=['POST',])
 def processing_edit():
-    pass
+    movie = Movies.query.filter_by(id=request.form['id']).first()
+    movie.name = request.form['name']
+    movie.date = datetime.strptime(request.form['date'], '%d/%m/%Y')
+    movie.grade1 = request.form['grade1']
+    movie.grade2 = request.form['grade2']
+
+    avg, sd = avg_sd(grade1=movie.grade1, grade2=movie.grade2)
+    calculus = Calculations.query.filter_by(id=request.form['id']).first()
+    calculus.avg = avg
+    calculus.sd = sd
+
+    db.session.add(movie)
+    db.session.commit()
+    db.session.add(calculus)
+    db.session.commit()
+
+    return redirect(url_for('index'))
