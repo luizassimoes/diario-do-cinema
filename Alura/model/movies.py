fbfc9d1d5de8dac40583edc9ce0713e559bef3a0
Luiza Souza Simoes <luiza.simoes@qualidados.com.br>
2024-01-18 10:16:34 -0300

    + coluna image em tabela Movies

--- Alura/model/movies.py
+++ Alura/model/movies.py
@@ -6,6 +6,7 @@ class Movies(db.Model):
     date = db.Column(db.DateTime)
     grade1 = db.Column(db.DECIMAL(10, 1))
     grade2 = db.Column(db.DECIMAL(10, 1))
+    image = db.Column(db.LargeBinary)       # Coluna para armazenar dados binários da imagem
 
     def __repr__(self):
         return '<Name %r>' % self.name
