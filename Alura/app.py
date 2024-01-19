ff27b5106797425d352d66ed9522801de90fed7d
Luiza Souza Simoes <luiza.simoes@qualidados.com.br>
2024-01-19 11:21:26 -0300

    +csrf = CSRFProtect(app)

--- Alura/app.py
+++ Alura/app.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')  # importa as variáveis globais (em CAPSLOCK)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+csrf = CSRFProtect(app)
 
 from views import *
 
